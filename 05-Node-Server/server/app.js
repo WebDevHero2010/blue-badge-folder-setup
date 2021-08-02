5c9e8a3c56861eb7ecda1bfaced4878d54deaad3
Marcus Elliott <melliott@pop-os.localdomain>
2021-08-02 12:11:54 -0400

    Latest Module folder

--- 05-Node-Server/server/app.js
+++ 05-Node-Server/server/app.js
@@ -1,27 +1,19 @@
 require("dotenv").config();
-let express = require('express');
+let express = require("express");
 let app = express();
+const sequelize = require("./db");
 
-// ***** ADD LINE BELOW
-let journal = require('./controllers/journalcontroller');
-let user = require('./controllers/usercontroller');
-
-const sequelize = require('./db');
-
-// ***** NEW CODE START *****
-// app.use('/test', function(req, res){
-//     res.send('This is a message from the test endpoint on the server!')
-// })
-// ***** NEW CODE END *****
+let journal = require("./controllers/journalcontroller");
+let user = require("./controllers/usercontroller");
 
 sequelize.sync();
 //sequelize.sync({force: true})
+app.use(require("./middleware/headers"));
 app.use(express.json());
 
-app.use('/user', user);
-app.use('/journal', journal);
+app.use("/user", user);
+app.use("/journal", journal);
 
-
-app.listen(3000, function(){
-    console.log('App is listening on port 3000')
-})
+app.listen(3000, function () {
+  console.log("App is listening on port 3000");
+});
