88daac62cf7dc7f2a28ed94f6e0f08be32411633
Marcus Elliott <melliott@pop-os.localdomain>
2021-08-03 21:20:48 -0400

    Server Challenge 8-3-2021

--- 05-Node-Server/server/db.js
+++ 05-Node-Server/server/db.js
@@ -1,15 +1,16 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('journal-walkthrough', 'postgres', 'password', {
-    host: 'localhost', dialect: 'postgres'
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("journal-walkthrough", "postgres", "password", {
+  host: "localhost",
+  dialect: "postgres",
 });
 
 sequelize.authenticate().then(
-    function() {
-        console.log('Connected to journal-walkthrough postgres database');
-    },
-    function(err){
-        console.log(err);
-    }
+  function () {
+    console.log("Connected to journal-walkthrough postgres database");
+  },
+  function (err) {
+    console.log(err);
+  }
 );
 
 module.exports = sequelize;
